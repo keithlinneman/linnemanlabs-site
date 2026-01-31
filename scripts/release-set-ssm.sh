fc3d2112ce98bbf10fd1281486b49a0b0acc15cd
Keith Linneman <keith@linnemanlabs.com>
2026-01-31 07:32:42 -0500

    checking s3 metadata on release step

--- scripts/release-set-ssm.sh
+++ scripts/release-set-ssm.sh
@@ -23,8 +23,15 @@ DEPLOY_ROLE_ARN="$( aws ssm get-parameter --name "${DEPLOY_ROLE_ARN_PARAM}" --qu
 : "${DEPLOY_ROLE_ARN:?failed to get DEPLOY_ROLE_ARN from SSM param ${DEPLOY_ROLE_ARN_PARAM}}"
 
 # validate that the file exists in s3
-aws s3 head-object --bucket "${RELEASE_BUCKET}" --key "${RELEASE_PREFIX}/${RELEASE_ID}.tar.gz" > /dev/null \
+aws s3api head-object --bucket "${RELEASE_BUCKET}" --key "${RELEASE_PREFIX}/${RELEASE_ID}.tar.gz" > /dev/null \
     || { echo "error: ${RELEASE_PREFIX}/${RELEASE_ID}.tar.gz not in S3 bucket ${RELEASE_BUCKET}"; exit 1; }
+
+s3_metadata="$( aws s3api head-object --bucket "${RELEASE_BUCKET}" --key "${RELEASE_PREFIX}/${RELEASE_ID}.tar.gz" )"
+s3_size="$( jq -r '.ContentLength' <<< "${s3_metadata}" )"
+[[ -z "${s3_size}" || "${s3_size}" == "null" ]] && { echo "error: ${RELEASE_PREFIX}/${RELEASE_ID}.tar.gz size: not in S3 bucket ${RELEASE_BUCKET}"; exit 1; }
+[[ "${s3_size}" -le 1024 ]] && { echo "error: ${RELEASE_PREFIX}/${RELEASE_ID}.tar.gz size is <1kb in S3 bucket ${RELEASE_BUCKET}"; exit 1; }
+s3_contenttype="$( jq -r '.ContentType' <<< "${s3_metadata}" )"
+[[ "${s3_contenttype}" != "application/x-tar" ]] && { echo "error: ${RELEASE_PREFIX}/${RELEASE_ID}.tar.gz in S3 bucket ${RELEASE_BUCKET} has invalid content-type ${s3_contenttype} expected application/x-tar"; exit 1; }
 
 # assume role in workload account in a subshell for cred isolation
 (
